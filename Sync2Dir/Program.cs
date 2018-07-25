f35bff00b6a9dd3d37838c8df7f8e6bedb92ce9f
AdamsterDomi <adamsterdomi@gmail.com>
2018-07-25 23:00:03 +0300

    Add project files.

---
+++ Sync2Dir/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Sync2Dir
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Let's sync two folders");
+            Console.WriteLine("Enter full path to folder 1:");
+            var folderA = Console.ReadLine();
+            Console.WriteLine("Enter full path to folder 2:");
+            var folderB = Console.ReadLine();
+        }
+    }
+}
